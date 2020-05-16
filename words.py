443f9f3db75071e7a601679f6de326c04c394f1d
spkuehl <skuehlhorn@gmail.com>
2020-05-15 23:11:36 -0500

    Initial WordComparison class. Passes most tests TODO comment identifies a bug to fix.

--- words.py
+++ words.py
@@ -1,3 +1,6 @@
+from nltk.stem.snowball import EnglishStemmer
+
+
 class WordComparison(object):
     def __init__(self, starting_word):
         self.starting_word = starting_word
@@ -7,4 +10,18 @@ class WordComparison(object):
         """Compare a starting word and a group of user submitted words.
            Return a list unique words, with matching user.
         """
-        pass
+        stemmer = EnglishStemmer()
+
+        #Make starting list
+        list_of_words = list(self.user_submissions.values())
+        list_of_words.append(self.starting_word)
+        #TODO Remove identical unique_words.
+        stemmed_dict = dict()
+        for word in list_of_words:
+            stemmed_dict[word] = stemmer.stem(word)
+
+        unique_words = [key for key, value in stemmed_dict.items()
+                        if list(stemmed_dict.values()).count(value) == 1]
+        if self.starting_word in unique_words:
+            unique_words.remove(self.starting_word)
+        return unique_words
