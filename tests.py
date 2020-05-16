5a4095a58547a77d2e1f5f7bf05f18750d15f6a1
spkuehl <skuehlhorn@gmail.com>
2020-05-15 21:20:46 -0500

    Initial commit with stubbed class and unit tests.

---
+++ tests.py
@@ -0,0 +1,16 @@
+import unittest
+import pytest
+from words import WordComparison
+
+
+def test_player_word_matches_starting_word():
+    pass
+
+def test_player_word_matches_player_word():
+    pass
+
+def test_player_word_matches_player_word_and_starting_word():
+    pass
+
+def test_all_words_unique():
+    pass
---
+++ words.py
@@ -0,0 +1,10 @@
+class WordComparison(object):
+    def __init__(self, starting_word):
+        self.starting_word = starting_word
+        self.user_submissions = dict()
+
+    def get_unique_words(self):
+        """Compare a starting word and a group of user submitted words.
+           Return a list unique words, with matching user.
+        """
+        pass
