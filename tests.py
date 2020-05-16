5708407542d8ce7757f4ed8e1e8922d7d425eb30
spkuehl <skuehlhorn@gmail.com>
2020-05-15 21:31:13 -0500

    Stub out more unit tests.

--- tests.py
+++ tests.py
@@ -2,14 +2,50 @@ import unittest
 import pytest
 from words import WordComparison
 
+# {'joe': 'ketchup'}
+#
+# {'joe': 'ketchup',
+#  'jane': 'ketchup'}
+#
+# {'joe': 'ketchup',
+#  'jane': 'ketchup'}
+#
+# {'joe': 'ketchup',
+#  'jane': 'mustard'}
+#
+# {'joe': 'princess'}
+#
+# {'joe': 'prince',
+#  'jane': 'pricess'}
+#
+# {'joe': 'prince',
+#  'jane': 'princess'}
+#
+# {'joe': 'princes'}
 
-def test_player_word_matches_starting_word():
+
+def test_player_word_matches_starting_word_identical():
     pass
 
-def test_player_word_matches_player_word():
+def test_player_word_matches_player_word_identical():
     pass
 
-def test_player_word_matches_player_word_and_starting_word():
+def test_player_word_matches_player_word_and_starting_word_identical():
+    pass
+
+def test_player_word_matches_starting_word_stemmed():
+    pass
+
+def test_player_word_matches_player_word_stemmed():
+    pass
+
+def test_player_word_matches_player_word_and_starting_word_stemmed():
+    pass
+
+def test_player_word_matches_starting_word_plural():
+    pass
+
+def test_player_word_matches_player_word_plural():
     pass
 
 def test_all_words_unique():
