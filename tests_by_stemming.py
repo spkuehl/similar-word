4b93cfbc8b09b4f9bcdfb49de34e4175e5f02816
spkuehl <skuehlhorn@gmail.com>
2020-05-27 19:09:12 -0500

    Unit tests to reflect updated method name, and change test filename.

--- tests_by_stemming.py
+++ tests_by_stemming.py
@@ -6,67 +6,67 @@ def test_all_words_unique():
     word_comp = WordComparison(prompt='condiment')
     word_comp.user_submissions = {'joe': 'ketchup',
                                   'jane': 'mustard'}
-    assert len(word_comp.get_unique_words()) == 2
+    assert len(word_comp.get_unique_words_by_stemming()) == 2
 
 def test_some_unique():
     word_comp = WordComparison(prompt='condiment')
     word_comp.user_submissions = {'joe': 'ketchup',
                                   'ash': 'ketchup',
                                   'jane': 'mustard'}
-    assert len(word_comp.get_unique_words()) == 1
+    assert len(word_comp.get_unique_words_by_stemming()) == 1
 
 def test_player_word_matches_prompt_identical():
     word_comp = WordComparison(prompt='condiment')
     word_comp.user_submissions = {'joe': 'condiment'}
-    assert len(word_comp.get_unique_words()) == 0
+    assert len(word_comp.get_unique_words_by_stemming()) == 0
 
 def test_player_word_matches_player_word_identical():
     word_comp = WordComparison(prompt='condiment')
     word_comp.user_submissions = {'joe': 'ketchup',
                                   'jane': 'ketchup'}
-    assert len(word_comp.get_unique_words()) == 0
+    assert len(word_comp.get_unique_words_by_stemming()) == 0
 
 def test_player_word_matches_player_word_and_prompt_identical():
     word_comp = WordComparison(prompt='ketchup')
     word_comp.user_submissions = {'joe': 'ketchup',
                                   'jane': 'ketchup'}
-    assert len(word_comp.get_unique_words()) == 0
+    assert len(word_comp.get_unique_words_by_stemming()) == 0
 
 def test_player_word_matches_prompt_stemmed():
     word_comp = WordComparison(prompt='wander')
     word_comp.user_submissions = {'joe': 'wanderer'}
-    assert len(word_comp.get_unique_words()) == 0
+    assert len(word_comp.get_unique_words_by_stemming()) == 0
 
 def test_player_word_matches_player_word_stemmed():
     word_comp = WordComparison(prompt='condiment')
     word_comp.user_submissions = {'joe': 'wander',
                                   'jane': 'wanderer'}
-    assert len(word_comp.get_unique_words()) == 0
+    assert len(word_comp.get_unique_words_by_stemming()) == 0
 
 def test_player_word_matches_player_word_and_prompt_stemmed():
     word_comp = WordComparison(prompt='wander')
     word_comp.user_submissions = {'joe': 'wanderer',
                                   'jane': 'wandering'}
-    assert len(word_comp.get_unique_words()) == 0
+    assert len(word_comp.get_unique_words_by_stemming()) == 0
 
 def test_player_word_matches_prompt_plural():
     word_comp = WordComparison(prompt='prince')
     word_comp.user_submissions = {'joe': 'princes'}
-    assert len(word_comp.get_unique_words()) == 0
+    assert len(word_comp.get_unique_words_by_stemming()) == 0
 
 def test_player_word_matches_player_word_plural():
     word_comp = WordComparison(prompt='condiment')
     word_comp.user_submissions = {'joe': 'prince',
                                   'jane': 'princes'}
-    assert len(word_comp.get_unique_words()) == 0
+    assert len(word_comp.get_unique_words_by_stemming()) == 0
 
 def test_player_word_matches_prompt_not_case_sensitive():
     word_comp = WordComparison(prompt='prince')
     word_comp.user_submissions = {'joe': 'PRInce'}
-    assert len(word_comp.get_unique_words()) == 0
+    assert len(word_comp.get_unique_words_by_stemming()) == 0
 
 def test_player_word_matches_player_word_not_case_sensitive():
     word_comp = WordComparison(prompt='condiment')
     word_comp.user_submissions = {'joe': 'Prince',
                                   'jane': 'pRINce'}
-    assert len(word_comp.get_unique_words()) == 0
+    assert len(word_comp.get_unique_words_by_stemming()) == 0
